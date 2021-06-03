91f78cfbc107c458f31178412be816abce48b8db
AlanNgaiii <634620668@qq.com>
2021-06-03 16:06:14 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="index-page">
+    <div class="wang-editor" ref="editor"></div>
+    <button @click="save">保存</button>
+    <div class="tv" v-html="content"></div>
+  </div>
+</template>
+
+<script>
+import Editor from "wangeditor";
+export default {
+  name: "editor",
+  data() {
+    return {
+      editor: null,
+      content: ""
+    };
+  },
+  methods: {
+    init() {
+      this.editor = new Editor(this.$refs["editor"]);
+      this.editor.create();
+    },
+    save() {
+      // this.$store.commit("setContent", { content: this.editor.txt.html() });
+      // this.$router.push({
+      //   path:"/tv"
+      // })
+      this.content = this.editor.txt.html();
+      console.log(this.content);
+    },
+  },
+  mounted() {
+    this.init();
+  },
+};
+</script>
+
+<style lang="scss">
+.index-page {
+  padding: 30px;
+}
+</style>
---
+++ src/views/tv.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tv" v-html="content"></div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "tv",
+  computed: {
+    ...mapState(["content"]),
+  },
+};
+</script>
